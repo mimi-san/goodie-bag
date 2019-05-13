2128c0fd7373018766acc84090d84b8a48fe415b
Maryia Miller <maryiamiller@gmail.com>
2019-05-13 10:48:17 -0400

    finished backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const router = require('express').Router()
+const {Candy} = require('../db/index')
 
 // Your routes go here!
 // NOTE: Any routes that you put here are ALREADY mounted on `/api`
@@ -18,6 +19,16 @@ const router = require('express').Router()
 // but you DON'T have a corresponding router, this piece of
 // middleware will generate a 404, and send it to your
 // error-handling endware!
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Candy.findAll();
+    res.send(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.use((req, res, next) => {
   const err = new Error('API route not found!')
   err.status = 404
--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,25 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
-module.exports = db.define('candy', {
-  // define your model here!
+const Candy = db.define('candy', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: true,
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    max: 10,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: true,
+  }
 });
+
+module.exports = Candy
